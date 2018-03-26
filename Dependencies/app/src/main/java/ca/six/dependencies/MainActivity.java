50d0847e3f51ce96bcd8de7043e0b01d9318482e
songzhw <songzhw2012@gmail.com>
2018-03-26 11:55:33 -0400

    dependencies

--- Dependencies/app/src/main/java/ca/six/dependencies/MainActivity.java
+++ Dependencies/app/src/main/java/ca/six/dependencies/MainActivity.java
@@ -24,5 +24,11 @@ compile project(path: ':lib')
 app可以使用lib中才导入了的EventBus
 
 2.
+app与lib中均依赖了: implementation "com.android.support:appcompat-v7:${supportVersion}"
+但只要版本号是一样的, 就不会有问题
+
+3.
+transivie?
+
 
  */
