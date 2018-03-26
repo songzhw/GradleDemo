91d2c865a3f5ef1e10376c7d110d59f33de95f4f
songzhw <songzhw2012@gmail.com>
2018-03-26 11:51:49 -0400

    dependencies

--- Dependencies/app/src/main/java/ca/six/dependencies/MainActivity.java
+++ Dependencies/app/src/main/java/ca/six/dependencies/MainActivity.java
@@ -3,11 +3,26 @@ package ca.six.dependencies;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import ca.six.lib.Bean;
+import de.greenrobot.event.EventBus;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Bean bean = new Bean();
+        EventBus eventBus = EventBus.getDefault();
     }
 }
+
+/*
+1.
+compile project(path: ':lib')
+app可以使用lib中才导入了的EventBus
+
+2.
+
+ */
