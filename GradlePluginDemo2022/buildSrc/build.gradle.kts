5fb4dd56449cfe1e08777f541d2a6115b9798a65
songzhw <songzhw2012@gmail.com>
2022-05-03 11:43:23 -0400

    error: "* What went wrong:         Task 's4' not found in root project 'GradlePluginDemo2022'."

--- GradlePluginDemo2022/buildSrc/build.gradle.kts
+++ GradlePluginDemo2022/buildSrc/build.gradle.kts
@@ -8,26 +8,7 @@ repositories {
     mavenCentral()
 }
 
-//import org.gradle.kotlin.dsl.`kotlin-dsl`
-//
-//repositories {
-//    mavenCentral()
-//}
-//plugins {
-//    kotlin("jvm") version "1.6.10"
-//    `kotlin-dsl`
-//    `java-gradle-plugin`
-//}
-//
-//dependencies {
-////    implementation gradleApi()
-////    implementation localGroovy()
-//    implementation "com.github.kittinunf.fuel:fuel:2.3.1"
-//    implementation "com.google.code.gson:gson:2.8.9"
-//}
-//
-//repositories {
-//    google()
-//    mavenCentral()
-//}
-//
+dependencies {
+    implementation("com.github.kittinunf.fuel:fuel:2.3.1")
+    implementation("com.google.code.gson:gson:2.8.9")
+}
--- GradlePluginDemo2022/buildSrc/src/main/kotlin/ca/six/gradles/me/MyInternalPlugin.kt
+++ GradlePluginDemo2022/buildSrc/src/main/kotlin/ca/six/gradles/me/MyInternalPlugin.kt
@@ -1,14 +1,15 @@
 package ca.six.gradles.me
 
+import com.github.kittinunf.fuel.httpGet
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class MyInternalPlugin: Plugin<Project>  {
     override fun apply(project: Project) {
-        project.task("s4") {
-            doLast{
-                println("szw << s4 task")
+        "http://192.168.2.246:8899/info?id=11"
+            .httpGet()
+            .responseString{req, resp, result ->
+                println(result.get())
             }
-        }
     }
 }
