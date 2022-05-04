23327475990355375798ad9dc8b78d8dfeeeef8a
songzhw <songzhw2012@gmail.com>
2022-05-04 08:47:10 -0400

    get task from extension

--- GradlePluginDemo2022/buildSrc/src/main/kotlin/ca/six/gradles/me/MyInternalTask.kt
+++ GradlePluginDemo2022/buildSrc/src/main/kotlin/ca/six/gradles/me/MyInternalTask.kt
@@ -8,8 +8,11 @@ open class MyInternalTask : DefaultTask() {
 
     @TaskAction
     fun callBackend() {
+        // 拿到extension实例
+        val ext = project.extensions.getByType(MyInternalExtension::class.java) // "project"来自DefaultTask中
+
         // fuel三方库, 用来Http请求
-        "http://192.168.2.246:8899/info?id=21"
+        "http://192.168.2.246:8899/info?id=${ext.id}"
             .httpGet()
             .responseString{req, resp, result ->
                 println(result.get())
